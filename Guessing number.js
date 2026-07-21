e12afa4d1b50cf313b168e7408c0cf9982548ec9
MD Abrar Jawad <abrarjawad.cse.216@gmail.com>
2026-07-22 02:39:42 +0600

    add count

--- Guessing number.js
+++ Guessing number.js
@@ -1,9 +1,11 @@
 let gameNum = Math.floor(Math.random() * 100) + 1;
 let userNum = prompt("Guess a Number between 1 and 100");
+let count = 0;
 
 {
     while (userNum != gameNum) 
     {
+        count++;
         if( userNum >= 1 && userNum <= 100)
         {
             if (userNum > gameNum) 
@@ -22,5 +24,5 @@ let userNum = prompt("Guess a Number between 1 and 100");
             userNum = prompt("Guess a Number between 1 and 100");
         }
     }
-    alert("Congratulations! You guessed the number.");            
+    alert("Congratulations! You guessed the number in " + count + " attempts.");            
 }
